e4289ab9b2ed03e7a718aeb9b666529f5ac2fc45
yangbranden <byang64@asu.edu>
2023-11-01 23:57:02 -0700

    ok so static HTML analysis is actually garbage

--- htmldom_analyzer.py
+++ htmldom_analyzer.py
@@ -2,21 +2,35 @@
 
 # Stage 3 - analyze HTML DOM
 # Features extracted from script:
-# 1. presence of <input> or <form> tag
-# 2. ratio of nil hrefs to total (href="#" attributes)
-# 3. redirect anchors
-# 4. 
+# X. Presence of <input> and <form> tag; all phishing pages have at least one text input
+# X. Presence of onclick, onload, onchange, onkeydown, onkeyup attributes
+# 1. Ratio of nil anchors (href=“#”) to all anchors (i.e. percentage of nil anchors)
+# 2. <head> element
+# 2. "href" attribute (<a>, <area>, <base>, <link>) outside of domain 
+# 3. "src" attribute (<audio>, <embed>, <iframe>, <img>, <input>, <script>, <source>, <track>, <video>) outside of domain
 
 import requests
 import os
 import csv
 from bs4 import BeautifulSoup
-    
-def parse_htmldom(response_content):
+
+def input_form_exists(response_content):
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response_content, 'html.parser') # response.content has the HTML DOM, we use bs4 to parse it
+    
+    input_tags = soup.find_all('input')
+    form_tags = soup.find_all('form')
+    onclick_elements = soup.find_all(attrs={"onclick": True})
+    onload_elements = soup.find_all(attrs={"onload": True})
+    onchange_elements = soup.find_all(attrs={"onchange": True})
+    onkeydown_elements = soup.find_all(attrs={"onkeydown": True})
+    onkeyup_elements = soup.find_all(attrs={"onkeyup": True})
+    
+    # print(soup.find_all("form"), soup.find_all("input"))
+    print(len(input_tags), len(form_tags), len(onclick_elements), len(onload_elements), len(onchange_elements), len(onkeydown_elements), len(onkeyup_elements))
+    
 
-# THIS IS TO GATHER THE RAW DATA BECAUSE PHISHING PAGES GET DELETED QUICKLY SO I AM GOING TO SAVE THE FULL DOM
+# Use simple GET request for HTML DOM
 def get_html_dom(url):
     # Send a GET request to the URL
     response = requests.get(url)
@@ -30,7 +44,7 @@ def get_html_dom(url):
         return None, False
 
 # Write row of CSV (append)
-def write_to_csv(filename, url, html_dom, result):
+def write_to_csv(filename, url, result):
     file_exists = os.path.exists(filename)
 
     # Read file if exists
@@ -52,11 +66,13 @@ def write_to_csv(filename, url, html_dom, result):
         if not file_exists:
             # Write header row
             csv_writer.writerow([
-                "website_url", "html_dom", "result"
+                "website_url", "result"
             ])
         
+        # values
+        
         row = [
-            url, html_dom, result
+            url, result
         ]
 
         csv_writer.writerow(row)
@@ -64,10 +80,11 @@ def write_to_csv(filename, url, html_dom, result):
         return True
 
 if __name__ == "__main__":
-    with open("phishing_sites.txt", "r") as f:
-        for url in f:
-            url = url.rstrip('\n')
-            html_dom, status = get_html_dom(url)
-            print(html_dom, status)
-            if status is True:
-                write_to_csv("raw_htmldom_data.csv", url, html_dom, True)
+    # website_url = input("Enter the website URL: ")
+    # html_dom, status = get_html_dom(website_url)
+    # input_form_exists(html_dom)
+    for i in range(10, 20):
+        with open(f"./raw_htmldom_data/{i}.html", "rb") as f:
+            input_form_exists(f.read())
+    # if status is True:
+    #     write_to_csv("htmldom_data.csv", website_url, html_dom, True)
--- thirdparty_analyzer.py
+++ thirdparty_analyzer.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
-# TODO
+# copypasting a script from another project; will add these in tomorrow
+from googleapiclient.discovery import build
+
+api_key = ""
+cse_id = ""
+query = ""
+
+def google_search(search_term, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res.get("items", None)
+
+# Google makes it so that you can't get more than 10 results at a time
+results = google_search(query, num=10)
+    
