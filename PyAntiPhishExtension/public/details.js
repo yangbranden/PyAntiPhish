b4127a145bee9eb7b4ff9f3e68584434d01b873c
yangbranden <byang64@asu.edu>
2023-12-01 13:22:05 -0700

    yay

---
+++ PyAntiPhishExtension/public/details.js
@@ -0,0 +1,7 @@
+function initPage() {
+    let logisticRegression = document.getElementById("logisticRegression");
+    let test = logisticRegression.getElementsByClassName("entryStatus");
+    test[0].innerHTML = "Clean";
+}
+
+initPage();
